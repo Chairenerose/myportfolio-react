82fa0684e18d4b13308c5ba0d7c1d0f3c78ce424
Chairenerose <76175773+Chairenerose@users.noreply.github.com>
2022-11-12 01:55:46 +0100

    Update Skills.js

--- src/Skills.js
+++ src/Skills.js
@@ -11,7 +11,7 @@ export default function Skills() {
                 <div className="web-developer">
                     <h3>Web Developer Skills</h3>
                     <ul>
-                        <li>HTML and CSS</li>
+                        <li>HTML/CSS</li>
                         <li>Responsive design</li>
                         <li>JavaScript/jQuery</li>
                         <li>JavaScript Library: React</li>
